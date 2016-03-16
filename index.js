719fd2dfc2d202a2744839e964a01509cd1486b2
fraserxu <xvfeng123@gmail.com>
2016-03-16 15:45:32 +1100

    clean up dependencies

--- index.js
+++ index.js
@@ -2,19 +2,12 @@
 
 var copy = require('copy-template-dir')
 var path = require('path')
-// var mkdirp = require('mkdirp')
 var argv = require('minimist')(process.argv.slice(2))
 
 var cwd = process.cwd()
 var templateDir = path.resolve(__dirname, 'template')
 var input = argv._[0]
 var targetDir = path.resolve(cwd, input)
-
-// mkdirp(targetDir, function (err) {
-//   if (err) {
-//     console.error(err)
-//     process.exit(1)
-//   }
 
 copy(templateDir, targetDir, {
   name: input
@@ -25,4 +18,3 @@ copy(templateDir, targetDir, {
   }
   console.log('Done. Happy hacking!')
 })
-// })
